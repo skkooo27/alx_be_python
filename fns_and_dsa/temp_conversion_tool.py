e89045f8989561954cc8cce169ec6b0af8d96699
skkooo27 <sharlinekerubo27@gamil.com>
2025-07-26 18:58:52 +0300

    Add functions for arithmetic ops, shopping list, datetime, and temp conversion

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,24 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+
+def convert_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+def convert_to_fahrenheit(celsius):
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+
+if __name__ == "__main__":
+    try:
+        temperature = float(input("Enter the temperature to convert: "))
+        unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
+
+        if unit == 'F':
+            result = convert_to_celsius(temperature)
+            print(f"{temperature}°F is {result}°C")
+        elif unit == 'C':
+            result = convert_to_fahrenheit(temperature)
+            print(f"{temperature}°C is {result}°F")
+        else:
+            print("Invalid unit. Please enter 'C' or 'F'.")
+    except ValueError:
+        raise ValueError("Invalid temperature. Please enter a numeric value.")
