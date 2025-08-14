f2e39e1e3df90f01b949182cf8e9533e81868a3b
skkooo27 <sharlinekerubo27@gamil.com>
2025-08-14 12:12:28 +0300

    Implement Calculator class with static and class methods

--- oop/main.py
+++ oop/main.py
@@ -2,6 +2,7 @@ from book_class import Book
 from library_system import Book, EBook, PrintBook, Library
 from polymorphism_demo import Shape, Rectangle, Circle
 import math
+from class_static_methods_demo import Calculator
 
 def main():
     # Creating an instance of Book
@@ -49,6 +50,19 @@ def main():
 
     for shape in shapes:
         print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+
+if __name__ == "__main__":
+    main()
+
+#task 3
+def main():
+    # Using the static method
+    sum_result = Calculator.add(10, 5)
+    print(f"The sum is: {sum_result}")
+
+    # Using the class method
+    product_result = Calculator.multiply(10, 5)
+    print(f"The product is: {product_result}")
 
 if __name__ == "__main__":
     main()
